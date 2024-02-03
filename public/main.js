94b40474267013bdd38c7381f5265248dd8e105e
Terren <11605395+terreng@users.noreply.github.com>
2024-02-02 20:00:02 -0500

    homepage

--- public/main.js
+++ public/main.js
@@ -8,18 +8,27 @@ function checkState() {
     gid("nav_rules").classList.remove("active");
     gid("nav_leaderboard").classList.remove("active");
 
+    gid("container_home").style.display = "none";
+    gid("container_history").style.display = "none";
+    gid("container_rules").style.display = "none";
+    gid("container_leaderboard").style.display = "none";
+
     if (document.location.pathname == "/") {
         openHome();
         gid("nav_home").classList.add("active");
+        gid("container_home").style.display = "block";
     } else if (document.location.pathname == "/history") {
         openHistory();
         gid("nav_history").classList.add("active");
+        gid("container_history").style.display = "block";
     } else if (document.location.pathname == "/rules") {
         openRules();
         gid("nav_rules").classList.add("active");
+        gid("container_rules").style.display = "block";
     } else if (document.location.pathname == "/leaderboard") {
         openLeaderboard();
         gid("nav_leaderboard").classList.add("active");
+        gid("container_leaderboard").style.display = "block";
     }
 }
 
